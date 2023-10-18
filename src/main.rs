beaf447a37ee3f51fa2cb8642ec65cadd694ad63
Sellsword9 <yerayromeroortiz@gmail.com>
2023-10-18 15:48:23 +0200

    Added more functions

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,33 @@ async fn add(path: web::Path<(i32, i32)>) -> impl Responder {
     let result = a + b;
     HttpResponse::Ok().body(result.to_string())
 }
-
+#[get("/multiply/{a}/{b}")]
+async fn multiply(path: web::Path<(i32, i32)>) -> impl Responder {
+    let (a, b) = path.into_inner();
+    let result = a * b;
+    HttpResponse::Ok().body(result.to_string())
+}
+#[get("/divide/{a}/{b}")]
+async fn divide(path: web::Path<(i32, i32)>) -> impl Responder {
+    let (a, b) = path.into_inner();
+    let result = a / b;
+    HttpResponse::Ok().body(result.to_string())
+}
+#[get("/pow/{a}/{b}")]
+async fn pow(path: web::Path<(i32, i32)>) -> impl Responder {
+    let (a, b) = path.into_inner();
+    let b = b as u32;
+    let result = a.pow(b);
+    HttpResponse::Ok().body(result.to_string())
+}
+#[get("/nrt/{a}/{b}")]
+async fn nrt(path: web::Path<(i32, i32)>) -> impl Responder {
+    let (a, b) = path.into_inner();
+    let b = b as f32;
+    let a = a as f32;
+    let result = a.powf(1.0/b);
+    HttpResponse::Ok().body(result.to_string())
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -21,6 +47,10 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(add)
             .service(subtract)
+            .service(multiply)
+            .service(divide)    
+            .service(pow) 
+            .service(nrt)
             .service(Files::new("/", "src").index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
--- src/script.js
+++ src/script.js
@@ -21,9 +21,56 @@ async function restar() {
     }
 }
 
+async function multiplicar() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+
+    if (num1 !== "" && num2 !== "") {
+        const response = await fetch(`/multiply/${num1}/${num2}`);
+        const result = await response.text();
+        document.getElementById("resultado").textContent = result;
+    }
+}
+
+async function dividir() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+
+    if (num1 !== "" && num2 !== "") {
+        const response = await fetch(`/divide/${num1}/${num2}`);
+        const result = await response.text();
+        document.getElementById("resultado").textContent = result;
+    }
+}
+
+async function potencia() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+
+    if (num1 !== "" && num2 !== "") {
+        const response = await fetch(`/pow/${num1}/${num2}`);
+        const result = await response.text();
+        document.getElementById("resultado").textContent = result;
+    }
+}
+
+async function raiz() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+
+    if (num1 !== "" && num2 !== "") {
+        const response = await fetch(`/nrt/${num1}/${num2}`);
+        const result = await response.text();
+        document.getElementById("resultado").textContent = result;
+    }
+}
 function init() {
     document.getElementById("btnSumar").addEventListener("click", sumar);
     document.getElementById("btnRestar").addEventListener("click", restar);
+    document.getElementById("btnMultiplicar").addEventListener("click", multiplicar);
+    document.getElementById("btnDividir").addEventListener("click", dividir);
+    document.getElementById("btnPotencia").addEventListener("click", potencia);
+    document.getElementById("btnRaiz").addEventListener("click", raiz);
 }
 
 document.addEventListener("DOMContentLoaded", init);
